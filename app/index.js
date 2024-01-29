6120c5ea3e7a0f8f959cc19f60feb34da8c13abb
syi012 <syi012@ucr.edu>
2024-01-29 14:58:25 -0800

    Expo setup layout
    Just initialized Expo Go:
    1. Download Expo Go in the app store.
    2. Run "npm start" on the terminal.
    3. Scan the QR code to see live updates on your phone.
    Reference: https://www.youtube.com/watch?v=mJ3bGvy0WAY&t=1531s

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Frequency</Text>
+        </View>
+    )
+}
+
+export default Home;
