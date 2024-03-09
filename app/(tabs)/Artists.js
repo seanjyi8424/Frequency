7a53d3aac150307e8600f9120c19d1035d6f84f7
Alejandro Vargas <74114912+Avarg116@users.noreply.github.com>
2024-03-09 14:00:51 -0800

    edited Artists to include Spotify API functionality

--- app/(tabs)/Artists.js
+++ app/(tabs)/Artists.js
@@ -1,22 +1,62 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList, Image, Linking } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const artists = [
-  { name: 'Travis Scott', image: 'https://i.pinimg.com/564x/dd/98/0f/dd980f04686e408c977e7c9743245aa8.jpg' },
-  { name: 'Kanye West', image: 'https://i.pinimg.com/736x/43/5d/e1/435de14fb0d811031eb6af2476eebe45.jpg' },
-  { name: 'Drake', image: 'https://i.pinimg.com/736x/4c/74/19/4c741913cd43b2c92939e5637fd5e0cb.jpg' },
-  { name: 'Taylor Swift', image: 'https://i.pinimg.com/564x/37/57/37/375737f9a009f5bb3f7c9019473be7f7.jpg' },
-  { name: 'Sheck Wes', image: 'https://i.pinimg.com/564x/90/1a/08/901a08779a4fab582394e7324960f6d7.jpg' },
-  { name: 'Don Toliver', image: 'https://i.pinimg.com/564x/3b/c0/55/3bc0556f85774e9fcc1809fb3733b520.jpg' },
-  { name: 'Metro Boomin', image: 'https://i.pinimg.com/564x/59/44/41/59444186f97d92bc8245dbb61319a99a.jpg' },
-  { name: 'Playboi Carti', image: 'https://i.pinimg.com/736x/06/7d/f0/067df069ce4bad5a55e4d93664ccded8.jpg' },
-  { name: 'Ken Carson', image: 'https://i.pinimg.com/564x/78/e8/75/78e87504c64b5930043d0e081f212bf0.jpg' },
-];
+const client_id = '047f8ea696a0445abf64e1738a07ac71'; //change this to test on your own spotify dev account
+const client_secret = 'd02b717f549c4ed29744df3059b7a342'; //change this to test on your own spotify dev account
+global.Buffer = require('buffer').Buffer;
+
+//getAccessToken(): retrieves an access token for requests from the Spotify API using necessary authentication data.
+async function getAccessToken() {
+    const authUrl = 'https://accounts.spotify.com/api/token';
+    const credentials = Buffer.from(client_id + ':' + client_secret).toString('base64');
+    const bodyData = 'grant_type=client_credentials';
+
+    try {
+        const response = await fetch(authUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${credentials}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: bodyData
+        });
+        const data = await response.json();
+        return data.access_token;
+    } catch(error) {
+        console.error('Error getting access token:', error.message);
+        return null;
+    }
+}
+
+async function getPopularArtists(accessToken) {
+  const popularArtistsUrl = 'https://api.spotify.com/v1/browse/new-releases?limit=50'; //new releases
+  try {
+      const response = await fetch(popularArtistsUrl, {
+          headers: { 
+              'Authorization': `Bearer ${accessToken}`,
+          },
+      });
+      const data = await response.json();
+      return data.albums.items.map(item => ({
+          name: item.artists[0].name,
+          image: item.images[0].url,
+          spotifyId: item.artists[0].id
+      }));
+  } catch(error) {
+      console.error('Error getting popular artists:', error);
+      return [];
+  }
+}
 
 const ArtistGridItem = ({ artist }) => {
+  const handlePress = () => {
+    const spotifyUrl = `https://open.spotify.com/artist/${artist.spotifyId}`;
+    Linking.openURL(spotifyUrl);
+  };
+
   return (
-    <TouchableOpacity style={styles.gridItem}>
+    <TouchableOpacity style={styles.gridItem} onPress={handlePress}>
       <Image source={{ uri: artist.image }} style={styles.artistImage} />
       <View style={styles.nameContainer}>
         <Text style={styles.artistName}>{artist.name}</Text>
@@ -26,50 +66,69 @@ const ArtistGridItem = ({ artist }) => {
 };
 
 const Artists = () => {
-    return (
+  const [popularArtists, setPopularArtists] = useState([]);
+
+  useEffect(() => {
+      async function fetchData() {
+          const accessToken = await getAccessToken();
+          const popularArtistsData = await getPopularArtists(accessToken);
+          setPopularArtists(popularArtistsData);
+      }
+      fetchData();
+  }, []);
+
+  return (
       <SafeAreaView style={styles.safeArea}>
+        <Text style={styles.title}>Browse Top Artists:</Text>
         <FlatList
-          data={artists}
+          data={popularArtists}
           renderItem={({ item }) => <ArtistGridItem artist={item} />}
           keyExtractor={(item) => item.name}
           numColumns={3}
-          style={styles.grid}
+          contentContainerStyle={styles.grid}
         />
       </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1, 
-    },
-    grid: {
-      flex: 1,
-    },
-    gridItem: {
-      flex: 1,
-      margin: 10,
-      aspectRatio: 1, // item is a square
-      justifyContent: 'center', // Center the content vertically 
-      alignItems: 'center', // Center the content horizontally 
-    },
-    artistImage: {
-      width: '100%', // full width of the grid item
-      aspectRatio: 1, // a square
-      height: '85%',
-      resizeMode: 'cover', // image covers the square without stretching
-    },
-    nameContainer: {
-      backgroundColor: 'white',
-      width: '100%',
-      padding: 5,
-      alignItems: 'center', //  text is centered within the name container
-    },
-    artistName: {
-      textAlign: 'center',
-      color: 'black',
-      fontSize: 16,
-    },
-  });  
+  safeArea: {
+      flex: 1, 
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  grid: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 10,
+    aspectRatio: 1, // item is a square
+    justifyContent: 'center', // Center the content vertically 
+    alignItems: 'center', // Center the content horizontally 
+  },
+  artistImage: {
+    width: '100%', // full width of the grid item
+    aspectRatio: 1, // a square
+    height: '85%',
+    resizeMode: 'cover', // image covers the square without stretching
+  },
+  nameContainer: {
+    backgroundColor: 'white',
+    width: '100%',
+    padding: 5,
+    alignItems: 'center', //  text is centered within the name container
+  },
+  artistName: {
+    textAlign: 'center',
+    color: 'black',
+    fontSize: 16,
+  },
+});  
 
 export default Artists;
