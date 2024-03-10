a9e9d080471f2faa737bfb69c05213535c89e966
aakashn14 <68211967+aakashn14@users.noreply.github.com>
2024-03-10 13:20:32 -0700

    Update index.js (firebase)

--- app/index.js
+++ app/index.js
@@ -1,7 +1,30 @@
-import { Redirect } from "expo-router";
+import React, { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { View, ActivityIndicator } from 'react-native';
 
 const StartPage = () => {
-    return <Redirect href={"/Login"} />;
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />; // Or some other loading indicator
+  }
+
+  if (!user) {
+    return <Redirect href="/Login" />;
+  } else {
+    return <Redirect href="/(tabs)" />; 
+  }
 };
 
 export default StartPage;
