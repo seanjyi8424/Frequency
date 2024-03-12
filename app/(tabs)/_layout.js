27bd45fdee143c52c5fddb241b4b33526a74e533
syi012 <syi012@ucr.edu>
2024-03-12 00:56:37 -0700

    Added Tab Icons

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
+import { Ionicons, Fontisto, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
@@ -10,11 +11,46 @@ export default function TabLayout() {
             headerStyle: {
               backgroundColor: '#f4511e',
             }, 
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="forum" color={color} size={24} />
+            ),
           }}/>
-        <Tabs.Screen name="Favorite" />
-        <Tabs.Screen name="Music" />
-        <Tabs.Screen name="Artists" />
-        <Tabs.Screen name="Upcoming" />
+        <Tabs.Screen 
+        name="Favorite"
+        options={{
+          title: 'Favorite',
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="favorite" color={color} size={20} />
+          ),
+        }} 
+        />
+        <Tabs.Screen 
+        name="Music" 
+        options={{
+          title: 'Music',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="musical-notes" color={color} size={24} />
+          ),
+        }} 
+        />
+        <Tabs.Screen 
+        name="Artists" 
+        options={{
+          title: 'Music',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="microphone-variant" color={color} size={24} />
+          ),
+        }} 
+        />
+        <Tabs.Screen 
+        name="Upcoming" 
+        options={{
+          title: 'Upcoming',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="upcoming" color={color} size={24} />
+          ),
+        }} 
+        />
         <Tabs.Screen name="Live" options={{ href: null }}/>
         <Tabs.Screen name="ComingSoon" options={{ href: null }}/>
     </Tabs>
